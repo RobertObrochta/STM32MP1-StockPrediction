9f00a9ee3d41904b0201fe9ed79c811f1bc7759a
RobertObrochta <robroch1@depaul.edu>
2021-09-13 10:50:22 -0500

    Refactored new.py

--- new.py
+++ new.py
@@ -47,12 +47,11 @@ scale = True
 loss_function = "mae"
 window_size = 50
 epochs = 850
+layers = 2
 
 '''
 End of Initialization Code ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 '''
-
-
 
 def shuffle_in_unison(a, b):
     state = np.random.get_state()
@@ -69,12 +68,12 @@ def gather_data(ticker = stock_ticker): # gathers data from yfin package, copies
     return result
 
 
-def prepare_data(ticker = stock_ticker, n_steps = window_size, scale = scale, shuffle = True, lookup_step = day_outlook, split_by_date = True, test_size = 0.2, feature_columns = ['Volume', 'Open', 'High', 'Low', "Close", "Adj Close"]):
+def prepare_data(ticker = stock_ticker, n_steps = window_size, scale = scale, shuffle = True, lookup_step = day_outlook, split_by_date = True, test_size = 0.2, feature_columns = ["Volume", "Open", "High", "Low", "Close", "Adj Close"]):
 
     gathered_data = gather_data()
 
     prepped_result = {}
-    prepped_result["Stock_data"] = gathered_data.copy()
+    prepped_result["Stock Data"] = gathered_data.copy()
 
     if scale:
         column_scaler = {}
@@ -83,67 +82,57 @@ def prepare_data(ticker = stock_ticker, n_steps = window_size, scale = scale, sh
 
         for column in feature_columns:
             scaler = preprocessing.MinMaxScaler()
-            gathered_data[column] = scaler.fit_transform(np.expand_dims(gathered_data[column].values, axis=1)
-            
-            )
+            gathered_data[column] = scaler.fit_transform(np.expand_dims(gathered_data[column].values, axis=1))
             column_scaler[column] = scaler
         # add the MinMaxScaler instances to the result returned
-        prepped_result["Column_scaler"] = column_scaler
+        prepped_result["Column Scaler"] = column_scaler
 
-    # add the target column by shifting by value of `lookup_step`
-    gathered_data['Future'] = gathered_data['Close'].shift(-lookup_step)
+    gathered_data["Future"] = gathered_data["Close"].shift(-lookup_step)
 
-    # # last `lookup_step` columns contains NaN in future column
-    # # get them before droping NaNs
     last_sequence = np.array(gathered_data[feature_columns].tail(lookup_step))
-    # # drop NaNs
+
     gathered_data.dropna(inplace=True)
     sequence_data = []
     sequences = deque(maxlen=n_steps)
-    for entry, target in zip(gathered_data[feature_columns].values, gathered_data['Future'].values): 
+    for entry, target in zip(gathered_data[feature_columns].values, gathered_data["Future"].values): 
         sequences.append(entry)
         if len(sequences) == n_steps:
             sequence_data.append([np.array(sequences), target])
 
-    # # get the last sequence by appending the last `n_step` sequence with `lookup_step` sequence
-    # # for instance, if n_steps=50 and lookup_step=10, last_sequence should be of 60 (that is 50+10) length
-    # # this last_sequence will be used to predict future stock prices that are not available in the dataset
     last_sequence = list([s[:len(feature_columns)] for s in sequences]) + list(last_sequence)
     last_sequence = np.array(last_sequence).astype(np.float32)
+    prepped_result["last_sequence"] = last_sequence
 
-    # # add to result
-    prepped_result['last_sequence'] = last_sequence
-
-    # # construct the X's and y's
-    X, y = [], []
+    # X and Y
+    X, Y = [], []
     for seq, target in sequence_data:
         X.append(seq)
-        y.append(target)
+        Y.append(target)
     # # # convert to numpy arrays
     X = np.array(X)
-    y = np.array(y)
+    Y = np.array(Y)
     if split_by_date:
     #     # split the dataset into training & testing sets by date (not randomly splitting)
         train_samples = int((1 - test_size) * len(X))
         prepped_result["X_train"] = X[:train_samples]
-        prepped_result["Y_train"] = y[:train_samples]
+        prepped_result["Y_train"] = Y[:train_samples]
         prepped_result["X_test"]  = X[train_samples:]
-        prepped_result["Y_test"]  = y[train_samples:]
+        prepped_result["Y_test"]  = Y[train_samples:]
         if shuffle:
     #         # shuffle the datasets for training (if shuffle parameter is set)
             shuffle_in_unison(prepped_result["X_train"], prepped_result["Y_train"])
             shuffle_in_unison(prepped_result["X_test"], prepped_result["Y_test"])
     else:    
         # split the dataset randomly
-        prepped_result["X_train"], prepped_result["X_test"], prepped_result["Y_train"], prepped_result["Y_test"] = train_test_split(X, y, test_size=test_size, shuffle=shuffle)
+        prepped_result["X_train"], prepped_result["X_test"], prepped_result["Y_train"], prepped_result["Y_test"] = train_test_split(X, Y, test_size = test_size, shuffle = shuffle)
 
     # # # retrieve test features from the original dataframe
-    prepped_result["Test_stock_data"] = prepped_result["Stock_data"]
+    prepped_result["Test_stock_data"] = prepped_result["Stock Data"]
 
     return prepped_result
 
 
-def create_model(sequence_length = window_size, n_features = 6, units = 256, cell = LSTM, n_layers = 2, dropout = 0.4, loss = loss_function, optimizer = "adam", bidirectional = False):
+def create_model(sequence_length = window_size, n_features = 6, units = 256, cell = LSTM, n_layers = layers, dropout = 0.4, loss = loss_function, optimizer = "adam", bidirectional = False):
     model = Sequential()
     for i in range(0, n_layers):
         if i == 0: # initial layer
@@ -187,14 +176,14 @@ def get_final_df(model, data):
     Y_test = data["Y_test"]
 
     # perform prediction and get prices
-    y_pred = model.predict(X_test)
+    Y_pred = model.predict(X_test)
     if scale:
-        Y_test = np.squeeze(data["Column_scaler"]["Adj Close"].inverse_transform(np.expand_dims(Y_test, axis=0)))
-        y_pred = np.squeeze(data["Column_scaler"]["Adj Close"].inverse_transform(y_pred))
+        Y_test = np.squeeze(data["Column Scaler"]["Adj Close"].inverse_transform(np.expand_dims(Y_test, axis=0)))
+        Y_pred = np.squeeze(data["Column Scaler"]["Adj Close"].inverse_transform(Y_pred))
 
     test_df = data["Test_stock_data"]
     # add predicted future prices to the dataframe
-    test_df[f"Adjclose_{day_outlook}"] = y_pred
+    test_df[f"Adjclose_{day_outlook}"] = Y_pred
     # add true future prices to the dataframe
     test_df[f"True_adjclose_{day_outlook}"] = Y_test
     # sort the dataframe by date
@@ -220,18 +209,18 @@ def get_final_df(model, data):
 
 def predict(model, data):
     prediction_stats = [] # every piece of data we want will be in this array
-    last_sequence = data["last_sequence"][-50:]
-    last_sequence = np.expand_dims(last_sequence, axis=0)
+    last_sequence = data["last_sequence"][-window_size:]
+    last_sequence = np.expand_dims(last_sequence, axis = 0)
 
     prediction = model.predict(last_sequence)
 
-    predicted_high = data["Column_scaler"]["High"].inverse_transform(prediction)[0][0]
-    predicted_low = data["Column_scaler"]["Low"].inverse_transform(prediction)[0][0]
-    predicted_open = data["Column_scaler"]["Open"].inverse_transform(prediction)[0][0]
-    predicted_close = data["Column_scaler"]["Close"].inverse_transform(prediction)[0][0]
-    predicted_vol = data["Column_scaler"]["Volume"].inverse_transform(prediction)[0][0]
+    predicted_high = data["Column Scaler"]["High"].inverse_transform(prediction)[0][0]
+    predicted_low = data["Column Scaler"]["Low"].inverse_transform(prediction)[0][0]
+    predicted_open = data["Column Scaler"]["Open"].inverse_transform(prediction)[0][0]
+    predicted_close = data["Column Scaler"]["Close"].inverse_transform(prediction)[0][0]
+    predicted_vol = data["Column Scaler"]["Volume"].inverse_transform(prediction)[0][0]
     if scale:
-        predicted_price_adjclose = data["Column_scaler"]["Adj Close"].inverse_transform(prediction)[0][0]
+        predicted_price_adjclose = data["Column Scaler"]["Adj Close"].inverse_transform(prediction)[0][0]
     else:
         predicted_price_adjclose = prediction[0][0]
 
@@ -249,7 +238,7 @@ def predict(model, data):
 Driver Code ........................................................................................................................................................................
 '''
 data = prepare_data()
-data["Stock_data"].to_csv(data_filename)
+data["Stock Data"].to_csv(data_filename)
 
 model_descriptor = f"STM32MP1-{today_date}-{loss_function}-adam-lstm"
 model_file_name = os.path.join("results", f"STM-{today_date}.hdf5") 
@@ -266,7 +255,7 @@ model.load_weights(f"results/{model_descriptor}.hdf5")
 loss, mae = model.evaluate(data["X_test"], data["Y_test"], verbose = 1)
 
 if scale:
-    mean_absolute_error = data["Column_scaler"]["Adj Close"].inverse_transform([[mae]])[0][0]
+    mean_absolute_error = data["Column Scaler"]["Adj Close"].inverse_transform([[mae]])[0][0]
 else:
     mean_absolute_error = mae
 
@@ -278,8 +267,7 @@ future_close = predict(model, data)[3]
 future_price_adjclose = predict(model, data)[4]
 future_vol = int(predict(model, data)[5])
 
-# # we calculate the accuracy by counting the number of positive profits
-# accuracy_score = (len(final_df[final_df['sell_profit'] > 0]) + len(final_df[final_df['buy_profit'] > 0])) / len(final_df)
+# accuracy_score = (len(final_df[final_df["sell_profit"] > 0]) + len(final_df[final_df["buy_profit"] > 0])) / len(final_df)
 
 
 # print prediction, candlestick stats
