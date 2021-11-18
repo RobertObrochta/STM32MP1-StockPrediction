546ed4be2cdff448af4c5aa3220adda34bc77f93
RobertObrochta <robroch1@depaul.edu>
2021-11-18 14:08:47 -0600

    Add visual output on target

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # STM32MP1-StockPrediction
+
+## To-Do
+- [ ] Quit GTK Main Loop after timer of 30 seconds and/or on touch
+- [ ] Interpret tensor output as Adj. Close Price
+- [X] Add output to target via GTK
+- [X] Some light output styling
--- deployments/deploy.py
+++ deployments/deploy.py
@@ -1,7 +1,11 @@
 
+import time
 import numpy as np
 import matplotlib.pyplot as mpp
 import tflite_runtime.interpreter as tflite
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk, Gdk
 
 tflite_model = "STM-StockPrediction-2021-09-28.tflite" # this will be updated depending on whenever this demo will be launched
 
@@ -17,13 +21,61 @@ interpreter.set_tensor(input_details[0]["index"], input_data)
 
 interpreter.invoke()
 
-output_data = interpreter.get_tensor(output_details[0]["index"])
+output_data = interpreter.get_tensor(output_details[0]["index"]) # still need to interpret this in the context of an actual price
 results = np.squeeze(output_data)
-print("output_data (as a tensor) =",output_data)
+print("Output_data (as a tensor) =",output_data)
 
-fig, ax = mpp.subplots(1, figsize=(12,6))
+# Gtk object to make the final result appear on screen (for now, it is just the adj. closing price)
+class OutputText(Gtk.Window):
+    def __init__(self):
+        super().__init__(title = "StockPrediction")
+        hbox = Gtk.Box(spacing=20)
+        hbox.set_homogeneous(True)
+        gtk_provider = Gtk.CssProvider()
+        css = """#output {     
+                            background-color: #F5DFA7;
+                            font-size: 70px; 
+                            color: #628B97;
+                          }""" # styling the text
 
-mpp.show()
+
+        label = Gtk.Label(label = str(output_data))
+        label.set_name("output")
+
+        gtk_provider.load_from_data(css.encode())
+
+        hbox.add(label)
+        self.add(hbox)
+
+        gtk_context = Gtk.StyleContext()
+        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(), gtk_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+        
+
+
+window = OutputText()
+window.connect("destroy", Gtk.main_quit)
+window.show_all()
+window.fullscreen()
+# ADD: close application window after 30 seconds or on double touch
+Gtk.main()
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Commented out, matplotlib doesn't load up on the target device. Gonna find another way to show a result (using Gtk for now)
+'''
+# fig, ax = mpp.subplots(1, figsize=(12,6))
+
+# mpp.show()
 
 # # high/low lines
 # mpp.plot([x[idx], x[idx]], [val['low'], val['high']], color='black')
