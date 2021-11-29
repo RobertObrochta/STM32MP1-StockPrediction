6830ef8a7a82b49b77189f21070413341d48bb15
RobertObrochta <robroch1@depaul.edu>
2021-11-29 10:01:33 -0600

    Incorporated closing main loop on 30 seconds timeout

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # STM32MP1-StockPrediction
 
 ## To-Do
-- [ ] Quit GTK Main Loop after timer of 30 seconds and/or on touch
 - [ ] Interpret tensor output as Adj. Close Price
+- [X] Quit GTK Main Loop after timer of 30 seconds
 - [X] Add output to target via GTK
 - [X] Some light output styling
+- [X] Compartmentalized into Python functions
--- deployments/deploy.py
+++ deployments/deploy.py
@@ -5,30 +5,12 @@ import matplotlib.pyplot as mpp
 import tflite_runtime.interpreter as tflite
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk
-
-tflite_model = "STM-StockPrediction-2021-09-28.tflite" 
-
-interpreter = tflite.Interpreter(model_path = tflite_model)
-interpreter.allocate_tensors()
-
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-input_shape = input_details[0]["shape"]
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-interpreter.set_tensor(input_details[0]["index"], input_data)
-
-interpreter.invoke()
-
-output_data = interpreter.get_tensor(output_details[0]["index"]) # still need to interpret this in the context of an actual price
-results = np.squeeze(output_data) # can be adjusted for interpretation 
-print("Results (as a tensor) =", results)
+from gi.repository import Gtk, Gdk, GLib
 
 
 # Gtk object to make the final result appear on screen (for now, it is just the adj. closing price)
 class OutputText(Gtk.Window):
-    def __init__(self):
+    def __init__(self, results):
         super().__init__(title = "StockPrediction")
         hbox = Gtk.Box(spacing=20)
         hbox.set_homogeneous(True)
@@ -50,37 +32,39 @@ class OutputText(Gtk.Window):
 
         gtk_context = Gtk.StyleContext()
         gtk_context.add_provider_for_screen(Gdk.Screen.get_default(), gtk_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-        
 
 
-window = OutputText()
-window.connect("destroy", Gtk.main_quit)
-window.show_all()
-window.fullscreen()
-# ADD: close application window after 30 seconds or on double touch
-Gtk.main()
+def interpret_tflite():
+  tflite_model = "STM-StockPrediction-2021-09-28.tflite" 
+
+  interpreter = tflite.Interpreter(model_path = tflite_model)
+  interpreter.allocate_tensors()
+
+  input_details = interpreter.get_input_details()
+  output_details = interpreter.get_output_details()
+
+  input_shape = input_details[0]["shape"]
+  input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+  interpreter.set_tensor(input_details[0]["index"], input_data)
+
+  interpreter.invoke()
+
+  output_data = interpreter.get_tensor(output_details[0]["index"])
+  results = np.squeeze(output_data) # adjust for interpretation of price
+  print("Results (as a tensor) =", results)
+
+  return results
 
 
+def main():
+  invoke_interpreter = interpret_tflite()
+
+  window = OutputText(invoke_interpreter)
+  window.connect("destroy", Gtk.main_quit)
+  window.show_all()
+  GLib.timeout_add(30000, Gtk.main_quit, window) # exits the Gtk main loop after 30 seconds
+  window.fullscreen() 
+  Gtk.main()
 
 
-
-
-
-
-
-
-
-
-'''
-Commented out, matplotlib doesn't load up on the target device. Gonna find another way to show a result (using Gtk for now)
-'''
-# fig, ax = mpp.subplots(1, figsize=(12,6))
-
-# mpp.show()
-
-# # high/low lines
-# mpp.plot([x[idx], x[idx]], [val['low'], val['high']], color='black')
-# # open marker
-# mpp.plot([x[idx], x[idx]-0.1], [val['open'], val['open']], color='black')
-# # close marker
-# mpp.plot([x[idx], x[idx]+0.1], [val['close'], val['close']], color='black')
+main()
